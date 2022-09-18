ab94966458d1a8034134b7393daf63928d9ae359
Alberto Vara <alberto.vara@datadoghq.com>
2022-09-18 20:54:21 +0200

    feat: solution

--- 2022_10_pycones/01_happy_path/_binarysearch.pyx
+++ 2022_10_pycones/01_happy_path/_binarysearch.pyx
@@ -5,7 +5,7 @@ from libcpp.vector cimport vector
 from constants import SEARCH_LIST
 
 cdef extern from "src/binarysearch.hpp":
-    int binsearch(vector[int] l, int value, int low, int high)
+    int binsearch(vector[int] * l, int value, int low, int high)
 
 
 cdef int cython_binary_search(l: List[int], value: int, low: int = 0, high: int = -1):
@@ -22,8 +22,9 @@ cdef int cython_binary_search(l: List[int], value: int, low: int = 0, high: int 
 
 cpdef void benchmark_native_binary_search(loops=10):
     cdef int len_search_list = len(SEARCH_LIST)
+    cdef vector[int] my_list = SEARCH_LIST
     for i in range(0, loops):
-        binsearch(SEARCH_LIST, 66666, 0, len_search_list)
+        binsearch(&my_list, 66666, 0, len_search_list)
 
 cpdef void benchmark_cython_binary_search(loops: int = 10):
     cdef int len_search_list = len(SEARCH_LIST)
--- 2022_10_pycones/01_happy_path/src/binarysearch.hpp
+++ 2022_10_pycones/01_happy_path/src/binarysearch.hpp
@@ -3,12 +3,12 @@
 
 using namespace std;
 
-int binsearch(vector<int> my_list, int value, int low, int high) {
+int binsearch(vector<int>* my_list, int value, int low, int high) {
     if (high >= low) {
         int mid = (low + high) / 2;
-        if (my_list[mid] == value) {
+        if ((*my_list)[mid] == value) {
             return mid;
-        } else if (my_list[mid] > value) {
+        } else if ((*my_list)[mid] > value) {
             return binsearch(my_list, value, low, mid - 1);
         } else {
             return binsearch(my_list, value, mid + 1, high);
