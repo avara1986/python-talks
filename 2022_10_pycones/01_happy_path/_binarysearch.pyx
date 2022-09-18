418b0253c1ea7ad5f2424a3c7a958a23094284ae
Alberto Vara <alberto.vara@datadoghq.com>
2022-09-18 12:38:14 +0200

    feat: clean code

--- 2022_10_pycones/01_happy_path/_binarysearch.pyx
+++ 2022_10_pycones/01_happy_path/_binarysearch.pyx
@@ -1,12 +1,11 @@
 from typing import List
 
 from libcpp.vector cimport vector
-# from cython cimport view
+
 from constants import SEARCH_LIST
 
 cdef extern from "src/binarysearch.hpp":
-    # int binsearch(int* l, int value, int low, int high)
-    int binsearch(vector[int]& l, int value, int low, int high)
+    int binsearch(vector[int] l, int value, int low, int high)
 
 
 cdef int cython_binary_search(l: List[int], value: int, low: int = 0, high: int = -1):
@@ -21,21 +20,10 @@ cdef int cython_binary_search(l: List[int], value: int, low: int = 0, high: int 
     else:
         return -1
 
-
 def benchmark_native_binary_search(loops=10):
-    cdef vector[int] my_list = SEARCH_LIST
+    cdef int len_search_list = len(SEARCH_LIST)
     for i in range(0, loops):
-        print(binsearch(my_list, 66666, 0, len(SEARCH_LIST)))
-
-
-# cpdef benchmark_native_binary_search_array(loops: int = 10):
-#     cdef int size = 100000
-#     cdef int[:] my_slice = view.array(shape=(size,), itemsize=sizeof(int), format="i")
-#     for i in range(size):
-#         my_slice[i] = i
-#
-#     for i in range(0, loops):
-#         native_binary_search(&my_slice[0], 66666, 0, size)
+        binsearch(SEARCH_LIST, 66666, 0, len_search_list)
 
 def benchmark_cython_binary_search(loops: int = 10) -> None:
     cdef int len_search_list = len(SEARCH_LIST)
--- 2022_10_pycones/01_happy_path/run.sh
+++ 2022_10_pycones/01_happy_path/run.sh
@@ -2,17 +2,8 @@ rm results/*
 rm _binarysearch.cpp
 rm _binarysearch.cpython-310-x86_64-linux-gnu.so
 python setup.py build_ext --inplace
-cd src_rust/
-maturin develop
-cd ..
 python benchmark.py -f native_binary_search  -i 10 -o results/native_binary_search.json
 python benchmark.py -f cython_binary_search  -i 10 -o results/cython_binary_search.json
-python benchmark.py -f rust_binary_search  -i 10 -o results/rust_binary_search.json
 python benchmark.py -f binary_search  -i 10 -o results/binary_search.json
 
 python -m pyperf compare_to --table results/binary_search.json results/cython_binary_search.json results/native_binary_search.json
-
-#python benchmark.py -o results/native_binary_search.json
-#python benchmark.py -o results/cython_binary_search.json
-#python benchmark.py -o results/binary_search.json
-#python -m pyperf compare_to --table results/binary_search.json results/cython_binary_search.json
